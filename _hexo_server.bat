9eff782a2d8fa8f8820c5b4470f5cc5219d5dbb0
zx <zx@zx.com>
2019-07-19 02:32:40 +0800

    2019-7-19 02:32:31 修改脚本

--- _hexo_server.bat
+++ _hexo_server.bat
@@ -1,2 +1,14 @@
-@SET SCRIPTS_DIR=%~dp0
-%USERPROFILE%\AppData\Roaming\npm\hexo.cmd  server  --port=4000  --cwd=%SCRIPTS_DIR%
+@SET SCRIPTS__DIR=%~dp0
+@SET SCRIPTS_NAME=%~nx0
+@SET SCRIPTS_NAME=%~n0%~x0
+
+@ECHO SCHTASKS /Create /TN HEXO_SERVER_BAT /RU SYSTEM /SC ONSTART /TR "%SCRIPTS__DIR%%SCRIPTS_NAME%"
+
+%USERPROFILE%\AppData\Roaming\npm\hexo.cmd  server  --port=4000  --cwd=%SCRIPTS__DIR%
+
+@REM  Windows下设置开机自启动程序(不依赖账户登录)
+@REM  思路：创建一个任务计划，它以系统账户(SYSTEM)运行，它在开机时(ONSTART)运行。无需账户登录。
+@REM  命令： `SCHTASKS /Create /TN 任务名          /RU SYSTEM /SC ONSTART /TR 程序的路径和文件名`。
+@REM  示例： `SCHTASKS /Create /TN HEXO_SERVER_BAT /RU SYSTEM /SC ONSTART /TR "%SCRIPTS__DIR%%SCRIPTS_NAME%"`。
+@REM  解释：创建一个任务计划，名为`HEXO_SERVER_BAT`，运行命令是`%SCRIPTS__DIR%%SCRIPTS_NAME%`。
+@REM  注意：如果需要更细致的设置，请在`任务计划程序`里面进行设置。
